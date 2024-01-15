aa82e6d7b2284ec290f318ec4e75a105142eb56d
Henoc04 <nyaohenoc@yahoo.fr>
2024-01-15 15:31:00 +0100

    workflow 1

---
+++ script.py
@@ -0,0 +1,7 @@
+
+nombre1 = float(input("Entrez le premier nombre : "))
+nombre2 = float(input("Entrez le deuxième nombre : "))
+
+somme = nombre1 + nombre2
+
+print(f"La somme de {nombre1} et {nombre2} est : {somme}")
