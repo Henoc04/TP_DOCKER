d4cb4546d040a85e5154c5e93325b53399b316cf
Henoc04 <nyaohenoc@yahoo.fr>
2024-01-15 16:00:16 +0100

    workflow1

--- script.py
+++ script.py
@@ -1,19 +1,2 @@
-try:
-    # Demander à l'utilisateur d'entrer le premier nombre
-    nombre1 = float(input("Entrez le premier nombre : "))
-except ValueError:
-    print("Erreur : Veuillez entrer un nombre valide.")
-    exit(1)
 
-try:
-    # Demander à l'utilisateur d'entrer le deuxième nombre
-    nombre2 = float(input("Entrez le deuxième nombre : "))
-except ValueError:
-    print("Erreur : Veuillez entrer un nombre valide.")
-    exit(1)
-
-# Additionner les deux nombres
-somme = nombre1 + nombre2
-
-# Afficher le résultat
-print(f"La somme de {nombre1} et {nombre2} est : {somme}")
+print(f"La somme")
